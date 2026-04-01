96db8fb7d11250719a906aa370c5e6dca9fd6013
Faiaz <mdazmayeenfaiaz@gmail.com>
2026-04-01 15:56:54 +0200

    Fix Sanity image source typing

--- lib/sanity.image.ts
+++ lib/sanity.image.ts
@@ -1,11 +1,9 @@
 import imageUrlBuilder from "@sanity/image-url";
-import { dataset, projectId } from "./sanity.client";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { sanityClient } from "./sanity.client";
 
-const builder = imageUrlBuilder({
-  projectId,
-  dataset,
-});
+const builder = imageUrlBuilder(sanityClient);
 
-export function urlFor(source: unknown) {
+export function urlFor(source: SanityImageSource) {
   return builder.image(source);
 }
